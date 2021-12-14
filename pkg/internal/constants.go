93c98cbab3898a69f2f231c890a7e21e401febf6
Josiah Adenegan <startnow65@gmail.com>
2021-12-14 12:43:52 +0100

    Add controllers to protect the API Token
    Since the credentials required to interact with
    Cloudflare is retrieved from the API Token resource.
    We need to ensure that it is not deleted if it is still referenced

--- pkg/internal/constants.go
+++ pkg/internal/constants.go
@@ -2,5 +2,6 @@ package internal
 
 const (
 	DeleteCFResourceFinalizer = "finalizers.kubeflare.io/delete-cf-resource"
+	ProtectAPITokenFinalizer  = "finalizers.kubeflare.io/protect-apitoken"
 	ImportedIDAnnotation      = "crds.kubeflare.io/imported-id"
 )
